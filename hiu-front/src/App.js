3a8b37b47ae2674b6a20799de2b17c5cf2bcc08e
Sergio <sergiorajaohariniaina@gmail.com>
2024-03-16 13:21:13 +0300

    version 1

--- hiu-front/src/App.js
+++ hiu-front/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import { useState } from 'react';
+import Home from './Page/Home/Home';
 
 
 
 
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path='/' element={""} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </>
   )
