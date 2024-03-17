dfaf236f442f3fd4720d0aa5b427fb62e8ef5d8c
YohanAmbinintsoa <yohanrabepro@gmail.com>
2024-03-18 00:03:41 +0300

    Integration login

--- hiu-front/src/App.js
+++ hiu-front/src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import { useState } from 'react';
 import Home from './Page/Home/Home';
-
-
-
-
-
+import Exposition from './Composant/jsx/Exposition_form'
+import { NextUIProvider } from '@nextui-org/react';
+import Oeuvre from './Composant/jsx/Oeuvre_form';
+import Login from '../src/Page/Login/Login'
 function App() {
   return (
-    <>
+    <NextUIProvider>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/expo' element={<Exposition />} />
+        <Route path='/oeuvre' element={<Oeuvre />} />
       </Routes>
-    </>
+    </NextUIProvider>
   )
 }
 
---
+++ hiu-front/src/Composant/jsx/Oeuvre_form.jsx
@@ -0,0 +1,184 @@
+import React, { useEffect, useState } from "react";
+import '../css/oeuvre.css'
+import { Input } from "@nextui-org/react";
+import { Select, SelectSection, SelectItem } from "@nextui-org/react";
+import { Textarea } from "@nextui-org/react";
+import Uploader from "./Uploader";
+import axios from 'axios';
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+import {Spinner} from "@nextui-org/react";
+import { HiCheck } from "react-icons/hi2";
+import { HiExclamationTriangle } from "react-icons/hi2";
+function Oeuvre() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [formData, setFormData] = useState(new FormData());
+    const [loading,setLoading]=useState(false);
+    const [error,setError]=useState(null);
+    const [data,setData]=useState(null);
+
+    useEffect(async function () {
+        setLoading(true);
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:8080/api/v1/Expositions',
+        };
+            await axios.request(config)
+            .then((response) => {
+                setData(response.data.data[0]);
+                setLoading(false);
+                console.log(response.data.data[0]);
+            })
+    }
+    ,[]);
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+        handleOpen();
+        try {
+                let config = {
+                    method: 'post',
+                    maxBodyLength: Infinity,
+                    url: 'http://localhost:8080/api/v1/Oeuvres',
+                    data : formData,
+                    headers:{
+                        authorization:"Bearer "+localStorage.getItem("Token")
+                    }
+                };
+                    await axios.request(config)
+                    .then((response) => {
+                        setFormData(new FormData());
+                        if (response.data.error!=null) {
+                            setError(response.data.error);
+                        }
+                        console.log(JSON.stringify(response.data));
+                        setLoading(false);
+                    })
+            
+        } catch (error) {
+            console.error('Error inserting data:', error);
+        }
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        console.log(name+": "+value);
+        if (formData.get(name)!=null) {
+            formData.set(name,value);
+        } else {
+            formData.append(name, value);
+        }
+    };
+
+    const handleImageChange=(event)=>{
+        formData.append("image",event.target.files[0]);
+    }
+
+    const handleOpen = () => {
+        onOpen();
+    }
+    return (
+        <>
+            <section className='center h-screen flex items-center content-center'>
+                <div className="rounded-md shadow-large w-full grid place-items-center child">
+                    <div className="container-oeuvre">
+                        <h1 className="title">Exposez votre oeuvre.</h1>
+                        <p className="text">Faites partie d'une galerie virtuelle dynamique et présentez vos œuvres à un public captivé 
+                        <br />par la beauté de l'art.</p>
+                        <form onSubmit={handleSubmit} className="form" action="">
+                            <div className="input-container">
+                                <div className="left-form">
+                                    <div className="input_box">
+                                        <Input onChange={handleInputChange} isRequired type="text" name='titre' label="Titre" />
+                                    </div>
+                                    <div className="input_box">
+                                        <Select
+                                            name="exposition"
+                                            className='w-full'
+                                            isRequired
+                                            label="Exposition"
+                                            placeholder="Choisir une exposition"
+                                            description="Choisir une exposition."
+                                            onChange={handleInputChange}
+                                        >
+                                        {data ? 
+                                            data.map((d)=>(
+                                                <SelectItem key={d.id} value={d.id}>
+                                                    {d.titre}
+                                                </SelectItem>
+                                            ))
+                                            :
+                                            <SelectItem key="0" value="0">
+                                                
+                                            </SelectItem>
+                                        }
+                                        </Select>
+                                    </div>
+                                    <div className="input_box">
+                                        <Input onChange={handleInputChange} type="date" name='dateparution' label="Titre" />
+                                    </div>
+                                    <div className="input_box textArea">
+                                        <Textarea
+                                            onChange={handleInputChange}
+                                            className="descri w-full"
+                                            label="Description"
+                                            disableAutosize
+                                            name="description"
+                                            placeholder="Entrer une description"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="right-form">
+                                    <Uploader onChange={handleImageChange}/>
+                                    <div className="taille">
+                                        <Input onChange={handleInputChange} isRequired type="number" name='longueur' label="Longueur" />
+                                        <Input onChange={handleInputChange} isRequired type="number" name='largeur' label="Largeur" />
+                                    </div>
+                                </div>
+                            </div>
+                            <button className='submit' type="submit">
+                                    Creer
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+            <Modal
+                size='md'
+                isOpen={isOpen}
+                onClose={onClose}
+                isDismissable={false}
+            >
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1"></ModalHeader>
+                            <ModalBody className='flex items-center content-center'>
+                                {loading ? <Spinner /> :
+                                    <>
+                                    {error 
+                                    ?
+                                        <>
+                                            <span className='error'><HiExclamationTriangle className="icon"/></span>
+                                            <p></p>
+                                        </>
+                                        :
+                                        <>
+                                            <span className='success'><HiCheck className='icon' /></span>
+                                            <h1>Poins io</h1>
+                                        </>
+                                    }
+                                    </>
+                                }
+                            </ModalBody>
+                            <ModalFooter></ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+    );
+}
+
+export default Oeuvre;
